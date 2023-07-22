64ddb258b8aa2878b3172d4360dd607a83dbc629
Second2dream <139936519+Second2dream@users.noreply.github.com>
2023-07-22 03:13:35 +0000

    finish guessing_game end

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,8 +6,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    println!("The secret number is:{secret_number}");
 
     loop{
 
